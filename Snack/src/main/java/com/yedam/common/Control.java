23e7fdd5fb92fa1b2739685bab3ffd3aaa08d5a8
tmdals <tmdals0145@naver.com>
2024-01-12 16:36:58 +0900

    test3

---
+++ Snack/src/main/java/com/yedam/common/Control.java
@@ -0,0 +1,8 @@
+package com.yedam.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Control {
+	public void execute(HttpServletRequest req, HttpServletResponse resp);
+}
---
+++ Snack/src/main/java/com/yedam/common/DataSource.java
@@ -0,0 +1,30 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DataSource {
+	private static SqlSessionFactory sqlSessionFactory;
+
+	private DataSource() {
+
+	}
+
+	// SqlSessionFactory 반환.
+	public static SqlSessionFactory getInstance() {
+		String resource = "config/mybatis-config.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+		return sqlSessionFactory;
+	}
+}
