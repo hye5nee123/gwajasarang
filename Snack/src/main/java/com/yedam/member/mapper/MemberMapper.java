27f4f5ca25b9c8de0bdb48047119767d6352d38f
sinsu <sinsu@DESKTOP-2AHUPUC>
2024-01-14 20:42:45 +0900

    20240114 test

--- Snack/src/main/java/com/yedam/member/mapper/MemberMapper.java
+++ Snack/src/main/java/com/yedam/member/mapper/MemberMapper.java
@@ -2,11 +2,7 @@ package com.yedam.member.mapper;
 
 import java.util.List;
 import com.yedam.member.vo.MemberVO;
-import org.apache.ibatis.annotations.Param;
 
 public interface MemberMapper {
-	public MemberVO selectMember(@Param("id") String id, @Param("pw") String pw);
 	public List<MemberVO> selectList();
-	public int insertMember(MemberVO vo);
-	public int deleteMember(String id);
 }
--- Snack/src/main/java/com/yedam/member/serviceImpl/MemberServiceImpl.java
+++ Snack/src/main/java/com/yedam/member/serviceImpl/MemberServiceImpl.java
@@ -2,11 +2,9 @@ package com.yedam.member.serviceImpl;
 
 import javax.sql.DataSource;
 
-import org.apache.ibatis.session.SqlSession;
-
 import com.yedam.member.mapper.MemberMapper;
 
-public interface MemberServiceImpl implements MemberService {
+public interface MemberServiceImpl implements MemberService{
 	SqlSession session = DataSource.getInstance().openSession(true);
 	MemberMapper mapper = session.getMapper(MemberMapper.class);
 	
