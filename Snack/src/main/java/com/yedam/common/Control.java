4b42756d3c8798ca2530350b08b3d8b5a6587df6
sinsu <sinsu@DESKTOP-2AHUPUC>
2024-01-14 20:44:27 +0900

    20240114

---
+++ Snack/src/main/java/com/yedam/common/Control.java
@@ -0,0 +1,9 @@
+package com.yedam.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Control {
+	//이클립스
+	public void execute(HttpServletRequest req, HttpServletResponse resp);
+}
---
+++ Snack/src/main/java/com/yedam/common/FrontController.java
@@ -0,0 +1,46 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+	
+	Map<String, Control> map;
+
+	public FrontController() {
+		map = new HashMap<String, Control>();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+	
+		super.init(config);
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/json;charset=utf-8");
+
+		String url = req.getRequestURI(); // /BoardWeb/main.do
+		String context = req.getContextPath(); // /BoardWeb
+		String path = url.substring(context.length());
+		System.out.println(path);
+
+		Control ctrl = map.get(path);
+		ctrl.execute(req, resp);
+
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy() 호출.");
+	}
+}
