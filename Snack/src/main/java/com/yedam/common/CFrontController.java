40c3f06eadc7716399a6fa9303720bdb814061e6
kimhj365 <khj6428@gmail.com>
2024-01-15 17:51:52 +0900

    20240115 FrontController 분할

--- Snack/src/main/java/com/yedam/common/CFrontController.java
+++ Snack/src/main/java/com/yedam/common/CFrontController.java
@@ -5,12 +5,16 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 
+import com.yedam.main.command.mainPageControl;
+import com.yedam.member.command.LoginFormControl;
+
 public class CFrontController extends HttpServlet{
 
 	Map<String, Control> map = new HashMap<>();
 	
 	Map<String, Control> getMap(){
-		
+		map.put("/mainPage.do", new mainPageControl());
+		map.put("/loginForm.do", new LoginFormControl());
 		return map;
 	}
 }
