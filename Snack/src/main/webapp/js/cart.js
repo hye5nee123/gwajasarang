9ed804decdd165a14dc2dd7eb7496a5c408bda30
혜원 <152113818+hey5nee123@users.noreply.github.com>
2024-01-18 11:17:57 +0900

    충돌 해결.

--- Snack/src/main/webapp/js/cart.js
+++ Snack/src/main/webapp/js/cart.js
@@ -3,20 +3,24 @@
  */
 $(document).ready(cartList());
 
+//플러스,마이너스 버튼 이벤트 등록.
+// btnEvent(){
+//     $(".inc.qtybtn").on("click",function () { 
 
-
+//      })
+// };
 
 //카트 전체 목록 보기
 function cartList() {
-  fetch("cartListJson.do?memberCode=M-00001", {
-    method: "get",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      $(res).each((idx, cart) => {
-        let tr = `      <tr>
+    fetch("cartListJson.do?memberCode=M-00001", {
+        method: "get",
+        headers: { "Content-Type": "application/json" }
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            $(res).each((idx, cart) => {
+                let tr = `      <tr>
                                     <td class="shoping__cart__item">
                                         <img src="img/cart/cart-1.jpg" alt="">
                                         <h5>${cart.goodsName}</h5>
@@ -28,7 +32,7 @@ function cartList() {
                                         <div class="quantity">
                                         <div class="pro-qty">
                                                 <span class="dec qtybtn">-</span>
-                                                <input type="text" value=0 class="que">
+                                                <input type="text" value=${cart.quantity} class="que">
                                                 <span class="inc qtybtn" >+</span>
                                             </div>
                                         </div>
@@ -38,9 +42,7 @@ function cartList() {
                                     </td>
                                     <td class="shoping__cart__item__close">
                                         <span class="icon_close"></span>
-                                        
                                     </td>
-<<<<<<< HEAD
                                 </tr>`
                 let cartListTbody = document.querySelector('#cartListTbody');
                 cartListTbody.insertAdjacentHTML('afterend', tr);
@@ -86,78 +88,20 @@ function cartList() {
                 console.log(event.target);
                 fetch("addCart.do?cartCode=1")
                 .then(result=> result.json())
-                    .then(result =>{
+                .then(result =>{
                     if(result.retCode == "OK"){
-                        alert('삭제됨');
-                event.target.closest("tr").remove();
-//                        location.reload();
+                     event.target.   
+               
+//                      
                     }else(
-                        alert('삭제 중 오류 발생')
+                        alert('오류 발생');
                     )
                 })
             })
-=======
-                                </tr>`;
-        let cartListTbody = document.querySelector("#cartListTbody");
-        cartListTbody.insertAdjacentHTML("afterend", tr);
-      }
-      ); //end of foreach
 
->>>>>>> branch 'johyewon' of https://github.com/hey5nee123/gwajasarang.git
+            
 
-
-<<<<<<< HEAD
         }) //end of click().
         .catch(console.error);
 };
 
-=======
-      //마이너스 버튼.
-      $(".dec").on("click", function () {
-        console.log(event.target.nextElementSibling);
-        let result = event.target.nextElementSibling.value;
-        if (result > 0) {
-          event.target.nextElementSibling.value = parseInt(result) - 1;
-        } else {
-          return 0;
-        }
-        event.target.nextElementSibling.value;
-      }); //end of minusBtn
-
-      //플러스 버튼.
-      $(".inc").on("click", function () {
-        //console.log(event.target.nextElementSibling);
-        let result = event.target.previousElementSibling.value;
-        event.target.previousElementSibling.value = parseInt(result) + 1;
-        //sevent.target.previousElementSibling.value(++$(cart.quantity));
-      }); //end of plusBtn
-
-      //아이템 삭제.
-      $(".icon_close").on("click", function () {
-        console.log($(".icon_close"));
-        event.target.closest("tr").remove();
-      }); //end of 아이템 삭제.
-
-      //로그인 체크(시도중)
-      function onCart(){
-        let goodsCode = $("goodsCode").val();
-        let memberCode = $("memberCode").val();
-        
-        if(memberCode ==""){
-          alert("로그인 후 이용 가능합니다.");
-          location.href="http://localhost:7950/Snack/"
-        }
-      }//end of onCart()
-    
-
-      
-    }) //end of click().
-    .catch(console.error);
-}
-
-       
-    
-
-onCart();
-cartList();
->>>>>>> branch 'johyewon' of https://github.com/hey5nee123/gwajasarang.git
