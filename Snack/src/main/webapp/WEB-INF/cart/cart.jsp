c53397c9713395f237d33718fbb052c3fd0da423
혜원 <152113818+hey5nee123@users.noreply.github.com>
2024-01-18 17:39:43 +0900

    cart 코드는 모르지만 희망이 보입니다...열심히 할게요...

--- Snack/src/main/webapp/WEB-INF/cart/cart.jsp
+++ Snack/src/main/webapp/WEB-INF/cart/cart.jsp
@@ -53,8 +53,7 @@
                     <div class="shoping__checkout">
                         <h5>Cart Total</h5>
                         <ul>
-                            <li>Subtotal <span>$454.98</span></li>
-                            <li>Total <span>$454.98</span></li>
+                            <li>Total <span id="total">0</span></li>
                         </ul>
                         <a href="#" class="primary-btn">PROCEED TO CHECKOUT</a>
                     </div>
--- Snack/src/main/webapp/js/cart.js
+++ Snack/src/main/webapp/js/cart.js
@@ -13,7 +13,7 @@ $(document).ready(cartList());
 //카트 전체 목록 보기
 function cartList() {
     fetch("cartListJson.do?memberCode=M-00001", {
-        method: "get",
+        method: "post",
         headers: { "Content-Type": "application/json" }
     })
         .then(res => res.json())
@@ -32,76 +32,107 @@ function cartList() {
                                         <div class="quantity">
                                         <div class="pro-qty">
                                                 <span class="dec qtybtn">-</span>
-                                                <input type="text" value=${cart.quantity} class="que">
+                                                <input type="text" value=1 class="que">
                                                 <span class="inc qtybtn" >+</span>
                                             </div>
                                         </div>
                                     </td>
-                                    <td class="shoping__cart__total">
-                                     
+                                    <td class="shoping__cart__total " >
+                                    ${cart.price * cart.quantity}
+                                    <td><button id="updateBtn">수정</td>
                                     </td>
-                                    <td class="shoping__cart__item__close">
-                                        <span class="icon_close"></span>
-                                    </td>
-                                </tr>`
+                                    <td class="shoping__cart__item__close" id="delCart"">
+                                        <span class="icon_close" data-cartCode="${cart.cartCode}"></span>
+                                        </td>
+                                        </tr>`
                 let cartListTbody = document.querySelector('#cartListTbody');
                 cartListTbody.insertAdjacentHTML('afterend', tr);
+                console.log('카트번호 : ' + `${cart.cartCode}`);
+                console.log('수량 : ' + $('.que').val());
+                
             })//end of foreach
-            //마이너스 버튼.
-            $(".dec").on('click', function () {
-                console.log(event.target.nextElementSibling);
-                let result = event.target.nextElementSibling.value;
-                if (result > 0) {
-                 event.target.nextElementSibling.value = parseInt(result) - 1;
-                }else{
-                    return 0;
-                }
-                event.target.nextElementSibling.value;
+            const totalList = document.querySelectorAll(".shoping__cart__total");
+            let cartPrice = 0;
+            totalList.forEach(ele => {
+                cartPrice = cartPrice + parseInt(ele.innerHTML);
+            })
+            document.querySelector('#total').innerHTML = cartPrice;
 
-            }) //end of minusBtn
-            //플러스 버튼.
+            //플러스 버튼 작동 fun().
             $(".inc").on('click', function () {
                 //console.log(event.target.nextElementSibling);
                 let result = event.target.previousElementSibling.value;
                 event.target.previousElementSibling.value = parseInt(result) + 1;
-                //sevent.target.previousElementSibling.value(++$(cart.quantity));
+                let pbtnresult = parseInt(result) + 1;
+
+                let onePrice = $(this).closest("td").prev().text();
+                $(this).closest("td").next().text(pbtnresult * onePrice);
+                console.log(pbtnresult * onePrice);
+                
+                const totalList = document.querySelectorAll(".shoping__cart__total");
+                console.log(totalList[0].innerHTML);
+                let cartPrice = 0;
+                totalList.forEach(ele => {
+                    cartPrice = cartPrice + parseInt(ele.innerHTML);
+                })
+                document.querySelector('#total').innerHTML = cartPrice;
+                                //sevent.target.previousElementSibling.value(++$(cart.quantity));
             }) //end of plusBtn
 
             //아이템 삭제.
-            $(".icon_close").on('click',function(){
-                console.log(event.target);
-                fetch("removeCart.do?cartCode=1")
-                //응답 결과 받는 구문(controller)
-                .then(result=> result.json())
-                .then(result =>{
-                    if(result.retCode == "OK"){
-                        alert('삭제됨');
-                event.target.closest("tr").remove();
-//                        location.reload();
-                    }else(
-                        alert('삭제 중 오류 발생')
-                    )
+            $(".icon_close").on('click', function () {
+                let cartCode = $(this).data("cartcode");
+                console.log(cartCode);
+                fetch("removeCart.do?cartCode=" + cartCode)
+                    //응답 결과 받는 구문(controller)
+                    .then(result => result.json())
+                    .then(result => {
+                        if (result.retCode == "OK") {
+                            alert('삭제됨');
+                            // event.target.closest("tr").remove();
+                            location.reload();
+                        } else {
+                            alert('삭제 중 오류 발생')
+                        }
+                    })
+            })
+            //end of 아이템 삭제.
+            
+            //수량 수정.
+            $(".")
+
+            //마이너스 버튼 작동 fun().
+            $(".dec").on('click', function () {
+                //console.log(event.target.nextElementSibling.value);
+                let result = event.target.nextElementSibling.value;
+                if (result > 0) {
+                    event.target.nextElementSibling.value = parseInt(result) - 1;
+                } else {
+                    return 0;
+                }
+                console.log(parseInt(result) - 1);
+                let mbtnresult = parseInt(result) - 1;
+
+                let onePrice = $(this).closest("td").prev().text();
+                $(this).closest("td").next().text(mbtnresult * onePrice);
+
+                console.log(mbtnresult * onePrice);
+                const totalList = document.querySelectorAll(".shoping__cart__total");
+                console.log(totalList[0].innerHTML);
+                let cartPrice = 0;
+                totalList.forEach(ele => {
+                    cartPrice = cartPrice + parseInt(ele.innerHTML);
                 })
-            })//end of 아이템 삭제.
-            //아이템 추가.
-            $(".inc").on("click",function(){
-                console.log(event.target);
-                fetch("addCart.do?cartCode=1")
-                .then(result=> result.json())
-                .then(result =>{
-                    if(result.retCode == "OK"){
-                     event.target.   
-               
-//                      
-                    }else(
-                        alert('오류 발생');
-                    )
-                })
+                document.querySelector('#total').innerHTML = cartPrice;
+                //event.target.nextElementSibling.value;
+
+            }) //end of minusBtn
+
+            //total 가격 출력.
+            $('.total').on('click',function(){
+
             })
 
-            
-
-        }) //end of click().
+        }) //end of .then(res =>).
         .catch(console.error);
-};
-
+};//end of cartList();
