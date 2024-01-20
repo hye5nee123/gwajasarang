bb10094b11f290b42ce659c8afd31f443090b942
kimhj365 <khj6428@gmail.com>
2024-01-21 04:27:15 +0900

    20240121 상품평 수정, 삭제, 페이징 완료

--- Snack/src/main/java/com/yedam/review/command/ModReviewJson.java
+++ Snack/src/main/java/com/yedam/review/command/ModReviewJson.java
@@ -1,45 +1,61 @@
 package com.yedam.review.command;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.yedam.common.Control;
 import com.yedam.review.service.ReviewService;
 import com.yedam.review.serviceImpl.ReviewServiceImpl;
 import com.yedam.review.vo.ReviewVO;
 
-public class ModReviewControl implements Control {
+public class ModReviewJson implements Control {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		
+		try {
+			req.setCharacterEncoding("utf-8");
+			resp.setContentType("text/json;charset=utf-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 
 		ReviewVO vo = new ReviewVO();
-	
+
 		String reviewCode = req.getParameter("reviewCode");
 		String title = req.getParameter("title");
 		String content = req.getParameter("content");
 		String stars = req.getParameter("stars");
-		
+
 		vo.setReviewCode(Integer.parseInt(reviewCode));
 		vo.setTitle(title);
 		vo.setContent(content);
 		vo.setStars(Integer.parseInt(stars));
 
 		ReviewService svc = new ReviewServiceImpl();
-
+		Map<String, Object> map = new HashMap<>();		
+		
+		if(svc.modReview(vo)) {
+			map.put("retCode", "OK");
+		} else {
+			map.put("retCode", "NG");
+		}
+		
+		Gson gson = new GsonBuilder().create();
+		
 		try {
-			if (svc.modReview(vo)) {
-				System.out.println("수정 완료!");
-				resp.sendRedirect("getReview.do?reviewCode=" + vo.getReviewCode());
-			} else {
-				System.out.println("수정 실패!");
-				resp.sendRedirect("modReviewForm.do?reviewCode=" + reviewCode);
-			}
+			resp.getWriter().print(gson.toJson(map));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
--- Snack/src/main/webapp/WEB-INF/review/reviewListAjax.jsp
+++ Snack/src/main/webapp/WEB-INF/review/reviewListAjax.jsp
@@ -1,22 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>reviewListAjax.jsp</title>
 </head>
-<script	src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/getReviewAjax.js" defer></script>
-<script type="text/javascript" src="js/reviewListAjax.js" defer></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <body>
-<h1>${logId }</h1>
-	<div id="reviewList">
-
-	</div>
+	<div id="reviewList"></div>
 	<script>
-		showList(`${goodsCode}`, `${logId}`);
+		const goodsCode = `${goodsCode}`;
+		const logCode = `${logCode}`;
+		const logId = `${logId}`;
+	</script>
+	<script type="text/javascript" src="js/reviewListAjax.js"></script>
+	<script>
+		showList();	
 	</script>
 </body>
 </html>
