f361f36b46edeb9a9b4c9e5ce6d11de16214f8eb
혜원 <152113818+hey5nee123@users.noreply.github.com>
2024-01-22 11:51:25 +0900

    pull 받기 용.

---
+++ Snack/src/main/java/com/yedam/cart/command/AddOrdersControl.java
@@ -0,0 +1,48 @@
+package com.yedam.cart.command;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedam.common.Control;
+import com.yedam.orders.service.OrdersService;
+import com.yedam.orders.serviceImpl.OrdersServiceImpl;
+import com.yedam.orders.vo.OrdersVO;
+
+public class AddOrdersControl implements Control {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		OrdersVO vo = new OrdersVO();
+		req.getMethod().equals("GET");
+		String odrerAddress = req.getParameter("orderAddress");
+		String orderName = req.getParameter("orderName");
+		String orderPhone = req.getParameter("orderPhone");
+		String memo = req.getParameter("memo");
+		String deliveryFee = req.getParameter("deliveryFee");
+		String totalPrice = req.getParameter("total_price");
+		String orderDate = req.getParameter("orderDate");
+
+		vo.setOrderAddress(odrerAddress);
+		vo.setOrderName(orderName);
+		vo.setOrderPhone(orderPhone);
+		vo.setMemo(memo);
+		vo.setDeliveryFee(0);
+		vo.setTotalPrice(0);
+		vo.setOrderDate(orderDate);
+
+		OrdersService svc = new OrdersServiceImpl();
+		try {
+
+			if (svc.addOrders(vo)) {
+				resp.sendRedirect("boardList.do");
+			} else {
+				resp.sendRedirect("boardForm.do");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
